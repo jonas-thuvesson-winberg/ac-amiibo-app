72ac00261593d3b9e85dc8a475a3ae9ce6cfac6b
Jonas Thuvesson Winberg <jonasthuvesson@gmail.com>
2022-09-14 16:05:10 +0200

    latest again

--- scripts/index.js
+++ scripts/index.js
@@ -1,64 +1,56 @@
 const fs = require("fs");
 const fsExtra = require("fs-extra");
 const axios = require("axios");
+const util = require("util");
+const stream = require("stream");
+
+const pipeline = util.promisify(stream.pipeline);
 
 const createFileName = (characterId, characterName) => {
   return `./src/assets/cards/${characterId}-${characterName}.png`;
 };
 
-const writeFile = (fileName, buffer) => {
-  fs.writeFileSync(fileName, buffer);
+const writeFile = async (fileName, dataStream) => {
+  const writeStream = fs.createWriteStream(fileName);
+  return await pipeline(dataStream, writeStream);
 };
 
 //fsExtra.emptyDirSync("../src/assets/cards");
 
-const requests = [];
+const doWork = async () => {
+  const data = (
+    await axios.get(
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vRkbDvXTEDxtMrbHaLw0_SpO4zWskaL6lX_WkhTfwmZ_cifkTuPQwZkacJwSOO5i1geS6RMOYOW_4aq/pub",
+      { params: { output: "csv" } }
+    )
+  ).data;
+  let datas = [];
 
-axios
-  .get(
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRkbDvXTEDxtMrbHaLw0_SpO4zWskaL6lX_WkhTfwmZ_cifkTuPQwZkacJwSOO5i1geS6RMOYOW_4aq/pub",
-    { params: { output: "csv" } }
-  )
-  .then((response) => {
-    const imgUrls = [];
-    const rows = response.data.split("\n");
-    for (let row of rows.slice(1)) {
-      const columns = row
-        .split(",")
-        .map((i) => i.trim())
-        .map((i) => i.trim(","));
+  const rows = data.split("\n");
+  for (let row of rows.slice(1)) {
+    const columns = row
+      .split(",")
+      .map((i) => i.trim())
+      .map((i) => i.trim(","));
 
-      const imageUrl = columns[4];
-      if (imageUrl) {
-        imgUrls.push([createFileName(columns[0], columns[1]), imageUrl]);
-      }
-    }
-    return imgUrls;
-  })
-  .then((imgUrls) => {
-    for (const [fileName, url] of imgUrls) {
-      requests.push(
-        axios
-          .get(url, {
-            // encoding: null,
-            responseType: "arrayBuffer",
-            transformResponse: [
-              (data) => {
-                // console.log(fileName);
-                return [fileName, data];
-              },
-            ],
-          })
-          .then((res) => res.data)
-      );
-    }
+    const imgUrl = columns[4];
+    if (imgUrl) {
+      const imgData = (
+        await axios.get(imgUrl, {
+          responseType: "stream",
+        })
+      ).data;
 
-    return Promise.all(requests);
-  })
-  .then((responses) => {
-    for (const [fileName, buffer] of responses) {
-      console.log(fileName);
-      writeFile(fileName, buffer);
+      const imgName = createFileName(columns[0], columns[1]);
+      datas.push([imgName, imgData]);
     }
-    console.log("done!");
-  });
+  }
+
+  for (const [fileName, stream] of datas) {
+    console.log(fileName);
+    await writeFile(fileName, stream);
+  }
+  console.log("done!");
+};
+
+doWork();
